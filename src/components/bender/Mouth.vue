85d4ed5a82a85b86c893a6edc01bfca080f28345
HollyCreep <lucasferreirarm@gmail.com>
2020-09-29 01:05:11 -0300

    balao indignaçao

--- src/components/bender/Mouth.vue
+++ src/components/bender/Mouth.vue
@@ -1,19 +1,22 @@
 <template>
-  <div
-    class="Bender__mouth"
-    :class="`mouth_state_${humor}`"
-  >
-    <template
-      v-for="n in 7"
+  <div class="d-flex flex-column align-center">
+    <div
+      class="Bender__mouth"
+      :class="`mouth_state_${humor}`"
     >
-      <div
-        :key="n"
-        class="Bender__tooth"
-      />
-    </template>
+      <template
+        v-for="n in 7"
+      >
+        <div
+          :key="n"
+          class="Bender__tooth"
+        />
+      </template>
 
-    <div class="Bender__lip" />
-    <div class="Bender__lip" />
+      <div class="Bender__lip" />
+      <div class="Bender__lip" />
+    </div>
+    <balao-mensagem />
   </div>
 </template>
 
@@ -21,6 +24,9 @@
   import { mapGetters } from 'vuex'
   export default {
     name: 'BenderMouth',
+    components: {
+      BalaoMensagem: () => import('@/components/core/BalaoMensagem'),
+    },
     computed: {
       ...mapGetters({ humor: 'getBenderHumor', speaking: 'getBenderIsSpeaking' }),
     },
---
+++ src/components/core/BalaoMensagem.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-expand-transition>
+    <div
+      v-show="humor == 'normal'"
+      class="balao text-center"
+    >
+      <span class="text-h4">42 ?! 42 ?????!</span>
+    </div>
+  </v-expand-transition>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters({ humor: 'getBenderHumor' }),
+    },
+  }
+</script>
